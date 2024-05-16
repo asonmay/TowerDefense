7b6b10ea114697c44490928ae444234ab957be62
Mason Lee <asonmay09@gmail.com>
2024-05-15 19:02:07 -0700

    added an Edge class

---
+++ TowerDefense/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    public class Edge
+    {
+        public PathTile startingTile;
+        public PathTile endingTile;
+    }
+}
